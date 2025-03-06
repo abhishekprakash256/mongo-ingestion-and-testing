5a068ab6d6619a520b7d04a4095377f7aaf49fa1
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-06 17:03:20 -0500

    article data is not fetching

--- insert_data_mongo.py
+++ insert_data_mongo.py
@@ -14,9 +14,18 @@ MONGO_HOST_NAME = "localhost"
 #helper method instance
 db_helper = mongo_helper_kit.Helper_fun(MONGO_HOST_NAME)
 
+#make the database 
+#db_helper.make_database_and_collection(DB_NAME, COLLECTION_NAME)
 
-db_helper.make_database_and_collection(DB_NAME, COLLECTION_NAME)
+#insert the data
+#db_helper.insert_data(DB_NAME, COLLECTION_NAME,article_test_data )
 
 
+#get the data , change to return the data  
+db_helper.show_all_data(DB_NAME, COLLECTION_NAME)
 
 
+#get the article data 
+res = db_helper.show_article_data(DB_NAME, COLLECTION_NAME,"test1")
+
+print(res)
