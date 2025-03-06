ebfc709f28e6572acd65731f136eff029730140e
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-06 17:10:01 -0500

    test data fetched

--- insert_data_mongo.py
+++ insert_data_mongo.py
@@ -26,6 +26,6 @@ db_helper = mongo_helper_kit.Helper_fun(MONGO_HOST_NAME)
 
 
 #get the article data 
-res = db_helper.show_article_data(DB_NAME, COLLECTION_NAME,article_name="test1")
+res = db_helper.show_article_data(DB_NAME, COLLECTION_NAME, {'article_name':"test1"})
 
 print(res)
