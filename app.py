9cc209dc1b5f31c0085986732ef373fa69ae99a8
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-08 13:56:22 -0500

    flask data fetch fixed

--- app.py
+++ app.py
@@ -3,45 +3,41 @@ import json
 import os 
 from datetime import timedelta, datetime
 from flask import Flask, Response , render_template, request, jsonify, redirect, make_response , url_for, send_from_directory
-#from flask_cors import CORS  not needed as per local testing
 import mongo_helper_kit
-from bson.json_util import dumps
-from bson import ObjectId
+from bson import json_util
 
-#constansts
+
+
 #constansts
 DB_NAME = "test-main-database"
 COLLECTION_NAME = "test-article-collections"
 MONGO_HOST_NAME = "localhost"
 
+
+
 #helper method instance
 db_helper = mongo_helper_kit.Helper_fun(MONGO_HOST_NAME)
 
-#make the database 
 
+
+#make the database 
 app = Flask(__name__)
 
 
-# Custom function to serialize MongoDB documents
-def serialize_document(doc):
-	"""Convert MongoDB document ObjectId to string."""
-	if isinstance(doc, list):
-		return [serialize_document(d) for d in doc]
-	if isinstance(doc, dict):
-		doc["_id"] = str(doc["_id"])
-		return doc
-	return doc
 
 
-@app.route("/mongo/section/<category>/article/<article_name>" , methods=["GET"])
+@app.route("/mongo/section/<category>/article/<article_name>" , methods=["GET"])  #error for the string id 
 def getArticleData(category,article_name):
 	"""
 	The function to get the article data from particular category
 	"""
 
-	data = list(db_helper.get_article_data(DB_NAME, COLLECTION_NAME, category, article_name))
+	data = db_helper.get_article_data(DB_NAME, COLLECTION_NAME, category, article_name)
+	#print(data)
 
-	return data
+	article_data = json.loads(json_util.dumps(data))
+
+	return jsonify(article_data)
 	
 
 
@@ -63,9 +59,6 @@ def getSectionData(category):
 	data = db_helper.get_card_data(DB_NAME, COLLECTION_NAME, category, limit)
 
 	return jsonify(data)
-
-	
-
 
 
 
