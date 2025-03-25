3ea4452b908a105b45e4e0c1c7de628b061e46ba
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-24 21:39:55 -0400

    test data added

--- insert_pgsql_data.py
+++ insert_pgsql_data.py
@@ -11,11 +11,17 @@ db_helper = Db_Helper(session, engine)
 
 # Create a new user
 db_helper.create_user(username='abhi', password='Qwerty@1234', userhash='JHlks#5')
+db_helper.create_user(username='abhi2', password='Qwerty@1235', userhash='JHlks#6')
+db_helper.create_user(username='abhi3', password='Qwerty@1236', userhash='JHlks#7')
+
 
 # Fetch user details
 print(db_helper.get_user_password(username='abhi'))
+
 print(db_helper.get_user_hash(username='abhi'))
+
 print(db_helper.check_user_exists(username='abhi'))
 
-
 print(db_helper.get_all_users())
+
+
